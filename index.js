cde5eb320f00c569e2797f43ddbf9891e6b98bf7
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-07 08:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 }
 
 function deleteFromObjectByKey(object,key) {
-  var nobject = object
+  var nobject = Object.assign(object,{})
   delete nobject[key]
   return nobject
 }
