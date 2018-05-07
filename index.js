df4f2de0e87b5962870e0d06dc6eacb62e7d1d0d
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-07 08:21:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object,key,value) {
   var nobject = Object.assign({}, object, {[key]:value})
   return nobject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  Object.assign(object,{[key]:[value]})
+  return object
+}
