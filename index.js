3ff331d9e3b2aac1a5814008fa56ade4c10745e0
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-07 08:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object,key,value) {
-  var x = key
-  console.log(x)
-  
-  var nobject = Object.assign({}, object, {x:value})
+  var nobject = Object.assign({}, object, {key:value})
   return nobject
 }
